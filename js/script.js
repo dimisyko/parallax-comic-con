f9ccb222bcca27ba7309c2f7a3d8175d4ceaa95b
dimisyko <dimitri.syko@hotmail.com>
2022-05-16 23:13:31 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -11,13 +11,13 @@
 		main.style.height = LargeurContainer - (window.innerWidth - window.innerHeight) + 'px'
 	}
 	//---------------------------------------------------------------------------------------------------------------------
-	const img = document.querySelectorAll('.image')
 	function scrollY() {
-		var train = document.querySelector('.image-train'),
-			spider = document.getElementById('spiderman')
+		const img = document.querySelectorAll('.image')
+		const train = document.querySelector('.image-train')
+		const spider = document.getElementById('spiderman')
 
 		img.forEach(function (parallaxImg) {
-			var dataVitesse = parallaxImg.getAttribute('data-vitesse'),
+			const dataVitesse = parallaxImg.getAttribute('data-vitesse'),
 				images = parallaxImg.getBoundingClientRect().left - window.innerWidth / 2
 			if (images > window.innerWidth || -images > window.innerWidth) return
 			container.style.transform = 'translate3d(-' + window.pageYOffset + 'px, 0px, 0px)'
@@ -30,10 +30,10 @@
 	window.addEventListener('scroll', scrollY)
 	//---------------------------------------------------------------------------------------------------------------------
 	function rain() {
-		let canvas = document.querySelector('canvas')
-		let ctx = canvas.getContext('2d')
-		let w = window.innerWidth
-		let h = window.innerHeight
+		const canvas = document.querySelector('canvas')
+		const ctx = canvas.getContext('2d')
+		const w = window.innerWidth
+		const h = window.innerHeight
 		canvas.width = w
 		canvas.height = h
 
